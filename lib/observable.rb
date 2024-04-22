80c4c1c8f1f6a05bbe7d930e3ff6221153f0ceef
John Gallagher <john@synapticmishap.co.uk>
2024-04-22 05:03:12 +0100

    chore: initial commit

---
+++ lib/observable.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "observable/version"
+
+module Observable
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/observable/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Observable
+  VERSION = "0.1.0"
+end
