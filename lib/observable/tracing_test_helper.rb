caec56cc99aa655988346d447976b4fd70adb818
John Gallagher <john@synapticmishap.co.uk>
2025-09-27 11:27:23 +0100

    refactor: Rename test support files to lib folder

---
+++ lib/observable/tracing_test_helper.rb
@@ -0,0 +1,38 @@
+require_relative "persistence/trace_repo"
+require_relative "persistence/span_repo"
+
+module Observable
+  module TracingTestHelper
+    def traces
+      Observable::Persistence::TraceRepo.new(spans: finished_spans)
+    end
+
+    def spans
+      Observable::Persistence::SpanRepo.new(spans: finished_spans)
+    end
+
+    def finished_spans
+      open_telemetry_exporter.finished_spans
+    end
+
+    def open_telemetry_exporter
+      @open_telemetry_exporter ||= setup_opentelemetry_for_tests
+    end
+
+    private
+
+    def setup_opentelemetry_for_tests
+      require "opentelemetry/sdk"
+
+      exporter = OpenTelemetry::SDK::Trace::Export::InMemorySpanExporter.new
+      span_processor = OpenTelemetry::SDK::Trace::Export::SimpleSpanProcessor.new(exporter)
+
+      # Configure the tracer provider with our span processor
+      tracer_provider = OpenTelemetry::SDK::Trace::TracerProvider.new
+      tracer_provider.add_span_processor(span_processor)
+      OpenTelemetry.tracer_provider = tracer_provider
+
+      exporter
+    end
+  end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,7 +4,7 @@ $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 
 require "observable"
 require "minitest/autorun"
-require "support/tracing_test_helper"
+require "observable/tracing_test_helper"
 require "support/readable_diffs_helper"
 require "support/hash_matcher"
 require "support/opentelemetry"
@@ -12,5 +12,5 @@ require "support/opentelemetry"
 class Minitest::Test
   include ReadableDiffsHelper
   include HashMatcher
-  include TracingTestHelper
+  include Observable::TracingTestHelper
 end
